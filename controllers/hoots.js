f23cb2927cd2608c5ecc8c55b512095602ccde1f
zjyezaire <zaire.elleby@gmail.com>
2024-10-23 09:35:52 -0400

    added user and comment schema

---
+++ controllers/hoots.js
@@ -0,0 +1,38 @@
+router.get('/', async (req, res) => {
+    try {
+      const hoots = await Hoot.find({})
+        .populate('author')
+        .sort({ createdAt: 'desc' });
+      res.status(200).json(hoots);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+  router.get('/:hootId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId).populate('author');
+    res.status(200).json(hoot);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post('/:hootId/comments', async (req, res) => {
+  try {
+    req.body.author = req.user._id;
+    const hoot = await Hoot.findById(req.params.hootId);
+    hoot.comments.push(req.body);
+    await hoot.save();
+
+    // Find the newly created comment:
+    const newComment = hoot.comments[hoot.comments.length - 1];
+
+    newComment._doc.author = req.user;
+
+    // Respond with the newComment:
+    res.status(201).json(newComment);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
